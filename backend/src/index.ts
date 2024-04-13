092cb75862a16012007ff7df895520cc5aaddc54
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-13 23:05:15 +0530

    Initialize handlers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,32 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// POST /api/v1/user/signup
+// POST /api/v1/user/signin
+// POST /api/v1/blog
+// PUT /api/v1/blog
+// GET /api/v1/blog/:id
+// GET /api/v1/blog/bulk
 
-export default app
+app.post("/api/v1/signup", (c) => {
+  return c.text("signup route");
+});
+
+app.post("/api/v1/signin", (c) => {
+  return c.text("signin route");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("get blog route");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("signin route");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("signin route");
+});
