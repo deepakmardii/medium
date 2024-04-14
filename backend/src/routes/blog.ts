03068eeca4c2bdf38e6e5485328f8c76b5b918ff
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-14 22:04:02 +0530

    zod on blog routes

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { verify } from "hono/jwt";
+import { createBlogInput, updateBlogInput } from "@deepakmardi22/medium-common";
 
 export const blogRouter = new Hono<{
   Bindings: {
@@ -39,6 +40,15 @@ blogRouter.use("/*", async (c, next) => {
 
 blogRouter.post("/", async (c) => {
   const body = await c.req.json();
+
+  const { success } = createBlogInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Input not correct",
+    });
+  }
+
   const authorId = c.get("userId");
 
   const prisma = new PrismaClient({
@@ -60,6 +70,14 @@ blogRouter.post("/", async (c) => {
 
 blogRouter.put("/", async (c) => {
   const body = await c.req.json();
+
+  const { success } = updateBlogInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      message: "Input not correct",
+    });
+  }
 
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
