8cbfb80f6d1fef073691b0bb52b01050b2803c11
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-14 10:52:19 +0530

    Add JWT to signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
@@ -29,9 +31,11 @@ app.post("/api/v1/signup", async (c) => {
         password: body.password,
       },
     });
-    return c.text("jwt here");
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
   } catch (e) {
-    return c.status(403);
+    c.status(403);
+    return c.json({ error: "error while signing up" });
   }
 });
 
