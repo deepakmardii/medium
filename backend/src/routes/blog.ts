ab71e154b60e50f8f9700169e7fb19abd0fe03f9
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-14 19:04:32 +0530

    Update route

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -20,7 +20,7 @@ blogRouter.post("/", async (c) => {
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-  await prisma.blog.create({
+  const blog = await prisma.blog.create({
     data: {
       title: body.title,
       content: body.content,
@@ -28,11 +28,31 @@ blogRouter.post("/", async (c) => {
     },
   });
 
-  return c.text("signin route");
+  return c.json({
+    id: blog.id,
+  });
 });
 
-blogRouter.put("/", (c) => {
-  return c.text("signin route");
+blogRouter.put("/", async (c) => {
+  const body = await c.req.json();
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const blog = await prisma.blog.update({
+    where: {
+      id: body.id,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+
+  return c.json({
+    id: blog.id,
+  });
 });
 
 blogRouter.get("/", (c) => {
