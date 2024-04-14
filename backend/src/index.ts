4f82abd0552b4ca8d86cd9944d61ae4dbc4e5239
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-14 17:11:37 +0530

    better routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,9 @@ const app = new Hono<{
   };
 }>();
 
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
+
 app.use("/api/v1/blog/*", async (c, next) => {
   const jwt = c.req.header("Authorization");
   if (!jwt) {
