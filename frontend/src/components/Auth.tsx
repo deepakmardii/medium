6ebc7aa31dd1f6e03240c4829ef78bee7c51693b
Deepak Mardi <deepakmardi22@gmail.com>
2024-04-15 16:44:30 +0530

    signup screen

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -12,38 +12,46 @@ const Auth = ({ type }: { type: "signup" | "signin" }) => {
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
         <div>
-          <div className="text-3xl font-extrabold">Create an account</div>
-          <div className="text-slate-400">
-            Already have an account?
-            <Link className="underline pl-2" to={"/sighin"}>
-              Login
-            </Link>
+          <div className="p-8 text-center">
+            <div className="text-3xl font-extrabold">Create an account</div>
+            <div className="text-slate-400">
+              Already have an account?
+              <Link className="underline pl-2" to={"/sighin"}>
+                Login
+              </Link>
+            </div>
+          </div>
+          <div>
+            <LabelInput
+              label="Name"
+              placeholder="Deepak Mardi..."
+              onChange={(e) => {
+                setPostInputs((c) => ({ ...c, name: e.target.value }));
+              }}
+            />
+            <LabelInput
+              label="Username"
+              placeholder="deepakmardi@gmail.com"
+              onChange={(e) => {
+                setPostInputs((c) => ({ ...c, username: e.target.value }));
+              }}
+            />
+            <LabelInput
+              type="password"
+              label="Password"
+              placeholder="123456789"
+              onChange={(e) => {
+                setPostInputs((c) => ({ ...c, password: e.target.value }));
+              }}
+            />
+            <button
+              type="button"
+              className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              {type === "signup" ? "Sign Up" : "Sign In"}
+            </button>
           </div>
         </div>
-        <LabelInput
-          type="text"
-          label="Name"
-          placeholder="Deepak Mardi..."
-          onChange={(e) => {
-            setPostInputs((c) => ({ ...c, name: e.target.value }));
-          }}
-        />
-        <LabelInput
-          type="text"
-          label="Username"
-          placeholder="deepakmardi@gmail.com"
-          onChange={(e) => {
-            setPostInputs((c) => ({ ...c, username: e.target.value }));
-          }}
-        />
-        <LabelInput
-          type="password"
-          label="Password"
-          placeholder="password"
-          onChange={(e) => {
-            setPostInputs((c) => ({ ...c, password: e.target.value }));
-          }}
-        />
       </div>
     </div>
   );
@@ -55,20 +63,18 @@ interface LabelInputType {
   label: string;
   placeholder: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  type: string;
+  type?: string;
 }
 
 function LabelInput({ label, placeholder, onChange, type }: LabelInputType) {
   return (
     <div>
-      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-        {label}
-      </label>
+      <label className="block mb-2 font-bold text-gray-900 pt-5">{label}</label>
       <input
         onChange={onChange}
         type={type || "text"}
         id="first_name"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
         placeholder={placeholder}
         required
       />
